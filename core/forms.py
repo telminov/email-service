a0b04252f4856412757067856f50d71abec2621f
Arseny Kirichenko <mr.ars156@gmail.com>
2017-07-30 02:06:23 +0300

    #3 создал базовый интерфейс

--- core/forms.py
+++ core/forms.py
@@ -1,20 +1,34 @@
 from django import forms
+from django.conf import settings
+
+from email_devino.client import DevinoClient
+from email_devino.client import DevinoException
 
 
-class AddTask(forms.Form):
-    name = forms.CharField()
-    sender_email = forms.EmailField()
+class SendMessage(forms.Form):
+    recipient_name = forms.CharField()
+    recipient_email = forms.EmailField()
+    sender_email = forms.ChoiceField(widget=forms.Select)
     sender_name = forms.CharField()
     subject = forms.CharField()
     text = forms.CharField()
-    type_task = forms.IntegerField(required=False)
-    start = forms.DateTimeField(required=False)
-    end = forms.DateTimeField(required=False)
-    user_id = forms.CharField(required=False)
+    user_message_id = forms.CharField(required=False)
+    user_campaign_id = forms.CharField(required=False)
     template_id = forms.CharField(required=False)
-    duplicates = forms.NullBooleanField(required=False)
 
+    def __init__(self, *args, **kwargs):
+        super(SendMessage, self).__init__(*args, **kwargs)
 
-class ContactList(forms.Form):
-    id = forms.IntegerField()
-    included = forms.BooleanField()
+        try:
+            answer = DevinoClient(settings.DEVINO_LOGIN, settings.DEVINO_PASSWORD).get_addresses_sender()
+            emails = []
+
+            for data in answer.result:
+                if data['Confirmed'] is True:
+                    emails.append(data['SenderAddress'])
+
+            emails = [(email, email) for email in emails]
+            self.fields['sender_email'].choices = emails
+
+        except DevinoException:
+            pass
---
+++ core/views/views.py
@@ -0,0 +1,47 @@
+import json
+
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from email_devino.client import DevinoClient
+from email_devino.client import DevinoException
+
+from core import models
+from core import forms
+from core import consts
+
+
+class SendMessage(LoginRequiredMixin, FormView):
+    login_url = reverse_lazy('login')
+    redirect_field_name = reverse_lazy('send_message_interface')
+    form_class = forms.SendMessage
+    success_url = reverse_lazy('send_message_interface')
+    template_name = 'core/send_message.html'
+
+    def form_valid(self, form):
+        json_data = json.dumps(form.cleaned_data)
+        devino_request = models.DevinoRequest.objects.create(api_resource=consts.SEND_MESSAGE, data=json_data)
+
+        try:
+            answer = DevinoClient(settings.DEVINO_LOGIN, settings.DEVINO_PASSWORD).send_transactional_message(
+                **form.cleaned_data)
+            models.DevinoAnswer.objects.create(
+                code=answer.code,
+                description=answer.description,
+                result=answer.result,
+                request=devino_request,
+            )
+            messages.success(self.request, 'Message successfully delivered')
+        except DevinoException as ex:
+            models.DevinoAnswer.objects.create(
+                code=ex.error.code,
+                description=ex.error.description,
+                request=devino_request,
+                is_fail=True,
+            )
+            messages.error(self.request, 'Error in sending the request, pleate repeat')
+            return self.form_invalid(form)
+        return super(SendMessage, self).form_valid(form)
