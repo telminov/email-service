f880b28b34a5069c56472b972a8d6dbf4bb1a5b2
Arseny Kirichenko <mr.ars156@gmail.com>
2017-08-01 22:06:29 +0300

    #3 небольшие правки

--- core/forms.py
+++ core/forms.py
@@ -8,7 +8,8 @@ from email_devino.client import DevinoException
 class SendMessage(forms.Form):
     recipient_name = forms.CharField()
     recipient_email = forms.EmailField()
-    sender_email = forms.ChoiceField(widget=forms.Select)
+    sender_email = forms.ChoiceField(widget=forms.Select, choices=[('server is not available, reload the page',
+                                                                    'server is not available, reload the page')])
     sender_name = forms.CharField()
     subject = forms.CharField()
     text = forms.CharField()
